2f4c13c3d880aed8db1d4126e8e261f14c4ce302
rodcezar <rodrigo.cezar@gmail.com>
2020-05-18 00:59:08 -0300

    Last commit today
    Some updates to the angular client

--- src/main/resources/public/hello.js
+++ src/main/resources/public/hello.js
@@ -5,3 +5,13 @@ angular.module('demo', [])
             $scope.produtos = response.data;
         });
 });
+
+
+$scope.deleteProduto = function(produto) { // Delete a Shipwreck. Issues a DELETE to /api/v1/shipwrecks/:id
+    if (popupService.showPopup('Really delete this?')) {
+      produto.$delete(function() {
+        $scope.produtos = Produto.query(); 
+        $state.go('produtos');
+      });
+    }
+  };
